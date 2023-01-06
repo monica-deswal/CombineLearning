bd392ec94dd14639b3aa6ab2b07eea896478c6b5
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2023-01-06 20:59:33 +0530

    finished the network changes

--- Combine_Part1/Combine_Part1/ViewController.swift
+++ Combine_Part1/Combine_Part1/ViewController.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    
+    private var webService = WebService()
+    private var cancellable: AnyCancellable?
 
+    private var posts = [Post]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    let cellIdentifier = "PostTableViewCell"
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        
+        self.cancellable = self.webService.getPosts()
+            .catch { _ in Just(self.posts)}
+            .assign(to: \.posts, on: self)
     }
-
-    
-
 }
 
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        let post = self.posts[indexPath.row]
+        cell?.textLabel?.text = post.title
+        return cell ?? UITableViewCell()
+    }
+}
